1603dfc398c3edc0497110ed0d652b4446fb91a5
&! (bitandbang) <bitnb@subvertising.org>
2015-12-29 09:27:16 -0500

    Added gulp build and clean tasks, moved tw to source/

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
-var gulp = require('gulp');
+var gulp   = require('gulp');
 var deploy = require('gulp-gh-pages');
+var del    = require('del');
+
+gulp.task('build', function(){
+  return gulp.src('./source/**/*')
+    .pipe(gulp.dest('./upload'))
+});
+
+gulp.task('clean', function(){
+  return del('./upload/**/*')
+});
 
 gulp.task('upload', function() {
   return gulp.src('./upload/**/*')
